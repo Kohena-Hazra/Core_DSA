24097ed5c45fe0612a77dd46b7f3035189c82659
KOHENA HAZRA <KOHENA HAZRA@DESKTOP-1013L4O>
2025-07-31 20:17:00 +0530

    updated

--- DSAinjavacore/src/sliding_window/Longestsubsum.java
+++ DSAinjavacore/src/sliding_window/Longestsubsum.java
@@ -7,7 +7,7 @@ public static int longsum(int arr[],int s) {
 	{
 		curr+=arr[r];
 	
-	while(curr>=s)
+	while(curr>s)
 	{
 		l++;
 		curr-=arr[l];
@@ -19,8 +19,8 @@ return best;
 
 public static void main(String args[])
 {
-	int arr[]= {4, 5, 2, 0, 1, 8, 12, 3, 6, 9};
-	int s=15;
+	int arr[]= {1,4,5,3,2,1,6};
+	int s=6;
 	System.out.println("the length of the longest subarray is : "+longsum(arr,s));
 			
 	}
